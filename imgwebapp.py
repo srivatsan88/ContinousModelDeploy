911d759da8a1fa3d97f8eda11cf86b5734c90c30
fasih99 <59806488+fasih99@users.noreply.github.com>
2022-05-10 01:09:20 +0530

    Update imgwebapp.py

--- imgwebapp.py
+++ imgwebapp.py
@@ -1,6 +1,3 @@
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-
 import tensorflow as tf
 import numpy as np
 import streamlit as st
@@ -9,32 +6,59 @@ import requests
 from io import BytesIO
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
-st.title("Bean Image Classifier")
-st.text("Provide URL of bean Image for image classification")
+st.title("Potato Image Classifier")
+st.text("Provide an image of Potato Leaf")
+
+menu = ["Image","Camera"]
+choice = st.sidebar.selectbox("Menu",menu)
+
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model('/app/models/')
+  model = tf.keras.models.load_model('/app/models/1')
   return model
 
 with st.spinner('Loading Model Into Memory....'):
   model = load_model()
 
-classes=['angular_leaf_spot','bean_rust','healthy']
+classes=['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
 
-def decode_img(image):
-  img = tf.image.decode_jpeg(image, channels=3)  
-  img = tf.image.resize(img,[224,224])
-  return np.expand_dims(img, axis=0)
 
-path = st.text_input('Enter Image URL to Classify.. ','https://beanipm.pbgworks.org/sites/pbg-beanipm7/files/styles/picture_custom_user_wide_1x/public/AngularLeafSpotFig1a.jpg')
-if path is not None:
-    content = requests.get(path).content
+if choice == "Image":
+    st.subheader("Image")
+    file= st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+    if file is not None:
 
-    st.write("Predicted Class :")
-    with st.spinner('classifying.....'):
-      label =np.argmax(model.predict(decode_img(content)),axis=1)
-      st.write(classes[label[0]])    
-    st.write("")
-    image = Image.open(BytesIO(content))
-    st.image(image, caption='Classifying Bean Image', use_column_width=True)
+        image= Image.open(file)
+    
+        img_array = np.array(image)
+    
+        img_tensor = tf.cast(img_array, tf.float32)
+        img = tf.image.resize(img_tensor,[256,256])
+        img = np.expand_dims(img, axis = 0)
+        
+        st.write("Predicted Class :")
+        with st.spinner('classifying.....'):
+          pred = model.predict(img)
+          
+          label =np.argmax(pred,axis=1)
+          
+          confidence = "{:.2f}".format(100*np.max(pred))
+          
+          st.write(classes[label[0]]) 
+          
+          st.write("Confidence :", confidence) 
+          st.write("")
+          image = Image.open(file)
+          st.image(image, caption='Classifying Potato Image', width=400)
+
+
+elif choice == "Camera":
+  st.subheader("Camera")
+  
+  picture = st.camera_input("Take a picture")
+
+  if picture:
+      st.image(picture)
+
+
